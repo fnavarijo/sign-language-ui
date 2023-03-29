ac9af5f397560e979ff2f674daaa0aca18c46ad8
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-03-29 01:49:02 -0600

    testing

--- types/sanity-resources/Lection.ts
+++ types/sanity-resources/Lection.ts
@@ -1,11 +1,12 @@
-import { Block } from 'sanity-blocks-vue-component/dist/types';
+// import { Block } from 'sanity-blocks-vue-component/dist/types';
 
 import { SanityResouce } from './BaseTypes';
 
 export type Lection = {
   name: string;
   description: string;
-  content: Block[];
+  // content: Block[];
+  content: string[];
   sign: SanityResouce;
   video: string;
 };
