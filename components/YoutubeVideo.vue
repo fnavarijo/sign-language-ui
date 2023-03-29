d15d9413fa25b9d3cdd46fcbbcb3926d95511c48
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-03-29 01:44:46 -0600

    first version video

--- components/YoutubeVideo.vue
+++ components/YoutubeVideo.vue
@@ -1,33 +1,31 @@
 <script lang="ts" setup>
   import {onMounted, onBeforeUnmount} from 'vue'
+  // TODO: Add types for this library
   // TODO: Add Shimer for loading
   import YTPlayer from 'yt-player'
-
-  interface YoutubeVideoProps {
-    videoId?: string;
-  }
-
-  const props = defineProps<YoutubeVideoProps>()
-
   let player;
+
+  const props = defineProps({
+    videoId: {
+      type: String,
+      required: true
+    }
+  })
   
   onMounted(() => {
     player = new YTPlayer('#yt-video')
     player.setSize(1080, 607)
     player.load(props.videoId)
-    // if (props.videoId) {
-    // }
   })
 
   onBeforeUnmount(() => {
-    if (player) {
-      player.destroy()
-    }
+    player.destroy()
   })
 </script>
 
 <template>
-  <div id="yt-video" class="mx-auto min-h-[607px]">
+  <div id="yt-video" class="mx-auto min-h-[607px]" >
+    
   </div>
 </template>
 
