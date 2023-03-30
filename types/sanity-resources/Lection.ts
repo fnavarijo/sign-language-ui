d7d0832ac4eaee91951b8b28632cb0c5a9660eba
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-03-30 08:47:55 -0600

    Revert "chore: Trying removing the sanityblocks dep"
    This reverts commit 5474c3d2067f20848507b6a366eb6f3de963d504.

--- types/sanity-resources/Lection.ts
+++ types/sanity-resources/Lection.ts
@@ -1,12 +1,11 @@
-// import { Block } from 'sanity-blocks-vue-component/dist/types';
+import { Block } from 'sanity-blocks-vue-component/dist/types';
 
 import { SanityResouce } from './BaseTypes';
 
 export type Lection = {
   name: string;
   description: string;
-  // content: Block[];
-  content: string[];
+  content: Block[];
   sign: SanityResouce;
   video: string;
 };
