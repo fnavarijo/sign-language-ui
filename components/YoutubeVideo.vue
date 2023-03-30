40eb792ba1861043a3b4d059761ef042fbba1d86
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-03-30 08:49:12 -0600

    chore: Restoring working version of Player

--- components/YoutubeVideo.vue
+++ components/YoutubeVideo.vue
@@ -1,33 +1,28 @@
 <script lang="ts" setup>
   import {onMounted, onBeforeUnmount} from 'vue'
+  // TODO: Add types for this library
   // TODO: Add Shimer for loading
   import YTPlayer from 'yt-player'
-
-  interface YoutubeVideoProps {
-    videoId?: string;
-  }
-
-  const props = defineProps<YoutubeVideoProps>()
-
   let player: YTPlayer;
-  
-  onMounted(() => {
-    if (props.videoId) {
-      player = new YTPlayer('#yt-video')
-      player.setSize(1080, 607)
-      player.load(props.videoId)
+  const props = defineProps({
+    videoId: {
+      type: String,
+      required: true
     }
   })
-
+  
+  onMounted(() => {
+    player = new YTPlayer('#yt-video')
+    player.setSize(1080, 607)
+    player.load(props.videoId)
+  })
   onBeforeUnmount(() => {
-    if (player) {
-      player.destroy()
-    }
+    player.destroy()
   })
 </script>
 
 <template>
-  <div id="yt-video" class="mx-auto min-h-[607px]">
+  <div id="yt-video" class="mx-auto min-h-[607px]" >
+    
   </div>
 </template>
-
