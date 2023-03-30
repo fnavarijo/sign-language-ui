8fd38562ce30a9ecfe59a8a2ad939bc79d712efc
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-03-30 09:05:04 -0600

    feat: adding fallback load

--- components/YoutubeVideo.vue
+++ components/YoutubeVideo.vue
@@ -1,28 +1,36 @@
 <script lang="ts" setup>
-  import {onMounted, onBeforeUnmount} from 'vue'
-  // TODO: Add types for this library
-  // TODO: Add Shimer for loading
-  import YTPlayer from 'yt-player'
-  let player: YTPlayer;
-  const props = defineProps({
-    videoId: {
-      type: String,
-      required: true
-    }
-  })
-  
-  onMounted(() => {
-    player = new YTPlayer('#yt-video')
-    player.setSize(1080, 607)
-    player.load(props.videoId)
-  })
-  onBeforeUnmount(() => {
-    player.destroy()
-  })
+import { onMounted, onBeforeUnmount } from 'vue';
+// TODO: Add Shimer for loading
+import YTPlayer from 'yt-player';
+
+interface YoutubeVideoProps {
+  videoId?: string;
+}
+
+let player: YTPlayer;
+const playerDimensions = {
+  width: 1080,
+  height: 607,
+};
+
+const props = defineProps<YoutubeVideoProps>();
+
+onMounted(() => {
+  if (props.videoId) {
+    player = new YTPlayer('#yt-video');
+    player.setSize(playerDimensions.width, playerDimensions.height);
+    player.load(props.videoId);
+  }
+});
+onBeforeUnmount(() => {
+  player.destroy();
+});
 </script>
 
 <template>
-  <div id="yt-video" class="mx-auto min-h-[607px]" >
-    
-  </div>
+  <div
+    id="yt-video"
+    class="mx-auto min-h-[607px] bg-primary-400"
+    :class="`w-[${playerDimensions.width}px]`"
+  ></div>
 </template>
