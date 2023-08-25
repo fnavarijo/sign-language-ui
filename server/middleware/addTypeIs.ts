5df7337b32b3c807a5f937c12712b47a8f88b0f0
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-08-25 00:18:54 -0600

    feat: Building server api

--- server/middleware/addTypeIs.ts
+++
@@ -1,9 +0,0 @@
-import typeis from 'type-is';
-import { NodeMiddleware } from 'h3';
-
-const middleware: NodeMiddleware = (req) => {
-  // @ts-ignore Replicating express is()
-  req.is = (mimeType: string) => typeis(req, mimeType);
-};
-
-export default fromNodeMiddleware(middleware);
